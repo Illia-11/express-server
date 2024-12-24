7351539ae0e2054baf91e0aa50bcf2a1afa9013a
Illia Piddubnyi <illya2008zp@gmail.com>
2024-12-24 19:42:00 +0200

    Add update user route example

--- controllers/userController.js
+++ controllers/userController.js
@@ -11,7 +11,7 @@ module.exports.getUsers = async (request, response) => {
 };
 
 module.exports.getUser = async (req, res) => {
-  const { params: { userId } } = req;
+  const { params: { userId }} = req;
 
   const user = users.find((user) => user.id === +userId);
   res.send(user);
@@ -30,7 +30,7 @@ module.exports.createUser = async (req, res, next) => {
 }
 
 module.exports.deleteUser = async (req, res) => {
-  const { params: { userId } } = req;
+  const {params: {userId}} = req;
 
   const user = users.find((user) => user.id === +userId);
 
@@ -38,6 +38,32 @@ module.exports.deleteUser = async (req, res) => {
     users = users.filter((user) => user.id !== +userId);
     res.send(user);
   } else {
-    res.send("User not defined");
+    res.send('User not exist');
+  }
+}
+
+module.exports.updateUser = async (req, res) => {
+  const { user, params: {userId}} = req;
+
+  const foundUser = users.find((user) => user.id === +userId);
+
+  if(foundUser) {
+    let updatedUser;
+    users = users.map((userInDb) => {
+      if(userInDb.id === +userId) {
+        updatedUser = {
+          ...userInDb,
+          ...user
+        }
+
+        return updatedUser;
+      }
+
+      return userInDb;
+    });
+
+    res.send(updatedUser);
+  } else {
+    res.send('User not exist');
   }
 }
--- middlewares/usersMW.js
+++ middlewares/usersMW.js
@@ -1,20 +1,34 @@
-const { REGISTRATION_SCHEMA } = require("../validation/userSchemas");
+const {
+  REGISTRATION_SCHEMA,
+  UPDATE_USER_SCHEMA,
+} = require('../validation/userSchemas');
 
 module.exports.registrationValidationMW = async (req, res, next) => {
   // console.log(req.body); // дані з тіла запиту
 
-  // REGISTRATION_SCHEMA.validate(req.body)
-  //   .then((user) => {
-  //     req.user = user;
-  //     next();
-  //   })
-  //   .catch((error) => {
-  //     res.send(error.message);
-  //   });
+  //   REGISTRATION_SCHEMA.validate(req.body)
+  //     .then((user) => {
+  //       req.user = user;
+  //       next();
+  //     })
+  //     .catch((error) => {
+  //       res.send(error.message);
+  //     });
 
   try {
     const user = await REGISTRATION_SCHEMA.validate(req.body);
     req.user = user;
+    next();
+  } catch (error) {
+    res.send(error.message);
+  }
+};
+
+module.exports.updateUserMW = async (req, res, next) => {
+  try {
+    const { body } = req;
+
+    req.user = await UPDATE_USER_SCHEMA.validate(body);
     next();
   } catch (error) {
     res.send(error.message);
