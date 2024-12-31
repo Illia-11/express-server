055e2da4cb04f11c4132dfc0c896051ba4efc42e
Illia Piddubnyi <illya2008zp@gmail.com>
2024-12-31 12:04:20 +0200

    Add express.statice middleware usage

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const bodyParser = express.json();
 // підʼєднує до app на всі методи міддлвери обробки json даних
 app.use(bodyParser);
 
+// віддає статичні файли з папки public
+app.use(express.static("public"));
+
 // підʼєднує до app на всі методи міддлвери роутера
 app.use(rootRouter);
 
